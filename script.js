b3cdc6c9993b2930acbb49f9fb0b59d6af310795
CollectionofEG <148367269+Pepperhfj@users.noreply.github.com>
2025-05-10 19:06:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const heading = document.getElementById
+("question")
+
+function change(){
+document.getElementById("question").
+innerText = "I am very sorry that you feel that way. You know, things can change - so I hope you're doing better after a while, and I want to let you know that I'm very proud of everything you do! Stay safe <3"}
