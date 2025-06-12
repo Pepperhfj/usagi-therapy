f77f818e77c9285f9383359b592765a18bb005b8
CollectionofEG <148367269+Pepperhfj@users.noreply.github.com>
2025-06-12 18:39:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-const heading = document.getElementById
-("question")
+const yesButton = document.getElementById('yes');
+const noButton = document.getElementById('no');
 
-function change(){
-document.getElementById("question").
-innerText = "I am very sorry that you feel that way. You know, things can change - so I hope you're doing better after a while, and I want to let you know that I'm very proud of everything you do! Stay safe <3"}
+const backButton1 = document.getElementById('goBackComfort');
+const backButton2 = document.getElementById('goBackAlt');
+
+yesButton.onclick = revealMessageAlt;
+
+noButton.onclick = revealMessage2;
+
+if (backButton1) {
+    backButton1.onclick = goBack;
+}
+if (backButton2) {
+    backButton2.onclick = goBack;
+}
+
+function revealMessage2() {
+    let window = document.getElementById('window');
+    window.style.display = "none";
+
+    let message2 = document.getElementById('Message2');
+    message2.style.display = "block";
+}
+
+function revealMessageAlt() {
+    let window = document.getElementById('window');
+    window.style.display = "none";
+
+    let messageAlt = document.getElementById('MessageAlt');
+    messageAlt.style.display = "block";
+}
+
+function goBack() {
+    let message2 = document.getElementById('Message2');
+    message2.style.display = "none";
+
+    let messageAlt = document.getElementById('MessageAlt');
+    messageAlt.style.display = "none";
+
+    let window = document.getElementById('window');
+    window.style.display = "block";
+}
