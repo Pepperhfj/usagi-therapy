e3c04524fa9afd80050b35f561f2ecb70492df87
CollectionofEG <148367269+Pepperhfj@users.noreply.github.com>
2025-06-13 10:26:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ But anyways, thank you for seeing my site and code! I made this so that people c
 feel better about the circumstances they're in, and I made it Usagi Chiikawa style to make 
 it more wholesome and comforting. Thank you for supporting my work ! */
 
+// Get the ID of 'yes' and 'no' buttons !
 const yesButton = document.getElementById('yes');
 const noButton = document.getElementById('no');
 
+/* Get the ID of all the buttons 
+that lead you to the main menu ! */
 const backButton1 = document.getElementById('goBackComfort');
 const backButton2 = document.getElementById('goBackAlt');
 
+// Clicking 'yes' or 'no' will activate two functions :>
 yesButton.onclick = revealMessageAlt;
-
 noButton.onclick = revealMessage2;
 
+/* Script to check if the element exists and
+to activate the 'goBack' function once clicked ´꒳` */
 if (backButton1) {
     backButton1.onclick = goBack;
 }
@@ -20,6 +25,7 @@ if (backButton2) {
     backButton2.onclick = goBack;
 }
 
+// noButton function . . .
 function revealMessage2() {
     let window = document.getElementById('window');
     window.style.display = "none";
@@ -28,6 +34,7 @@ function revealMessage2() {
     message2.style.display = "block";
 }
 
+// yesButton function . . .
 function revealMessageAlt() {
     let window = document.getElementById('window');
     window.style.display = "none";
@@ -36,6 +43,7 @@ function revealMessageAlt() {
     messageAlt.style.display = "block";
 }
 
+// goBack function . . .
 function goBack() {
     let message2 = document.getElementById('Message2');
     message2.style.display = "none";
